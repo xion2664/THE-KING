472f163d053e1dba0624d1b87e6c3e1252d524be
xuubxxn <xuubxxn@gmail.com>
2021-07-20 20:12:29 +0900

    [BE] 수정::모델 이미지 필드 추가(유빈)

--- account/models.py
+++ account/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class User(AbstractUser) :
     nickname = models.CharField(max_length=50)
+    profile = models.ImageField(upload_to='account/', blank=True, null=True)
     big_score = models.IntegerField(default=0, verbose_name = "대변점수")
     small_score = models.IntegerField(default=0, verbose_name = "소변점수")
     gas_score = models.IntegerField(default=0, verbose_name = "방귀점수")
