ca41817a7500f4b47159fa4699fb2845e16fe074
윤병수 <qudtn990@likelion.org>
2021-07-20 21:19:20 +0900

    [BE] 개발 : urls, views (병수)

--- board/urls.py
+++ board/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('create/', views.upload, name="create"),
+    path('detail/<str:id>', views.detail, name="detail"),
+    path('edit/<str:id>', views.edit, name="edit"),
+    path('delete/<str:id>', views.delete, name="delete"),
 ]
