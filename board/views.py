88e830b6d3cf6ddc9aad031d1cdbbbe6e48dd712
윤병수 <qudtn990@likelion.org>
2021-07-27 18:30:44 +0900

    [BE] 개발 :: views.py 수정 (병수)

--- board/views.py
+++ board/views.py
@@ -33,7 +33,7 @@ def upload(request) :
     if request.method == "POST":
         # POST 방식일떄 새로운 글 생성
         new_board = Board()
-        new_board.choice= request.POST.get('choice')
+        new_board.choice= request.POST.get('type')
         new_board.created_at = timezone.datetime.now() 
         new_board.image = request.FILES.get('image') 
         new_board.body = request.POST.get('body')
