bb3c26bb2b4142096a184e8bff69afed965a0fa4
xuubxxn <xuubxxn@gmail.com>
2021-07-29 02:31:25 +0900

    [BE] 구현::history 구현(유빈)

--- account/models.py
+++ account/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from board.models import Board
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
@@ -9,3 +10,10 @@ class User(AbstractUser) :
     small_score = models.IntegerField(default=0, verbose_name = "소변점수")
     gas_score = models.IntegerField(default=0, verbose_name = "방귀점수")
     total_score = models.IntegerField(default=0, verbose_name = "총합")
+
+
+class history(models.Model) :
+    board = models.ForeignKey(Board, on_delete=models.CASCADE)
+
+    def __str__(self) :
+        return self.board.body
